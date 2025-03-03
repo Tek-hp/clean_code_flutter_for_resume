3ba68b7f3df0614362c09fbacb2ebd716291e669
Tek <tekrajojha6@gmail.com>
2025-03-03 23:39:00 +0800

    Removed lints

--- lib/src/features/home/presentation/pages/home_page.dart
+++ lib/src/features/home/presentation/pages/home_page.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-import 'package:resume/src/enums/selected_tab.dart';
 import 'package:resume/src/features/home/presentation/bloc/home_bloc.dart';
 import 'package:resume/src/features/home/presentation/widgets/current_tab.dart';
-import 'package:resume/src/features/home/presentation/widgets/header.dart';
-import 'package:resume/src/features/home/presentation/widgets/selection_tab.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
